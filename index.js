0154f712af27fa1347efc1ce2bc46b3f520429dd
NkoLow <104155407+NkoLow@users.noreply.github.com>
2023-10-17 14:26:57 -0700

    ça marche

--- index.js
+++ index.js
@@ -2,23 +2,22 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Serve static files from the "public" directory
-app.use(express.static("public"));
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
 
+// Dynamic endpoint that takes an argument
 app.get('/api/data/:arg', (req, res) => {
     const arg = req.params.arg;
+    // Call a separate function to generate a JSON response
     const result = generateResponse(arg);
     res.json(result);
 });
 
+// Separate function for generating the JSON response
 function generateResponse(arg) {
-    return { message: `This is the button ${arg}` };
+    // You can customize this function to generate a response based on 'arg'
+    return { message: `You requested data with argument: ${arg}` };
 }
-
-app.all('*', (req, res) =>
-    res.status(404)
-        .send('<h1>ERROR 404! Page not found!</h1>')
-);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
